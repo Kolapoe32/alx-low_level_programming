4974f213a22afd67a5eaa62d311d29e236470858
Oladunjoye Kolapo <Kolapo574@gmail.com>
2023-02-21 10:02:23 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alpha in lowercase
+ */
+void print_alphabet(void)
+{
+char ka;
+for (ka = 'a'; ka <= 'z'; ka++)
+_putchar(ka);
+
+_putchar('\n');
+}
