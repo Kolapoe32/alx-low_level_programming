72cfc92cf7207a5e702907d42313c15eabc6f6b1
Oladunjoye Kolapo <Kolapo574@gmail.com>
2023-02-23 15:52:30 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,7 +10,8 @@ void print_diagonal(int n)
 if (n <= 0)
 {
 _putchar('\n');
-} else
+}
+else
 {
 int i, j;
 
@@ -20,7 +21,7 @@ for (j = 0; j < n; j++)
 {	
 if (j == i)
 _putchar('\\');
-else if (j < i);
+else if (j < i)
 _putchar(' ');
 }
 _putchar('\n');
